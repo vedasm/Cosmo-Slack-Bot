c5dcbc34fdc4da9691f1e32bca28efa1ee7867d6
vedasm <sivamanimuthu.veda@gmail.com>
2026-06-06 07:33:19 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+require("dotenv").config();
+
+const { App } = require("@slack/bolt");
+const axios = require("axios");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true
+});
+
+app.command("/csb-ping", async ({ command, ack, respond }) => {
+  const start = Date.now();
+  await ack();
+  const latency = Date.now() - start;
+  await respond({ text: `Pong!\nLatency: ${latency}ms` });
+});
+
+app.command("/csb-catfact", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://catfact.ninja/fact");
+    await respond({ text: `Cat Fact:\n${response.data.fact}` });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a cat fact." });
+  }
+});
+
+app.command("/csb-joke", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://official-joke-api.appspot.com/random_joke");
+    await respond({
+      text:
+`${response.data.setup}
+
+${response.data.punchline}`
+    });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a joke." });
+  }
+});
+
+app.command("/csb-help", async ({ ack, respond }) => {
+  await ack();
+  await respond({
+    text:
+`Available Commands:
+/csb-ping - Check bot latency
+/csb-catfact - Get a cat fact
+/csb-joke - Tell u a Joke!`
+  });
+});
+
+(async () => {
+  await app.start();
+  console.log("bot is running!");
+})();
